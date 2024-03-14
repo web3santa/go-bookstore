d3f9852e73f19733187528c63752d3c23cbb9ba5
web3santa <usefulstory35@gmail.com>
2024-03-15 07:27:36 +0900

    finished

---
+++ cmd/main/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/web3santa/go-bookstore/pkg/routes"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":3000", r))
+
+}
